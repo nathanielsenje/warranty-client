d7bd540cd9b868dcb87388f50bf51bef50583bd3
Nathaniel Senje <117443449+nathanielsenje@users.noreply.github.com>
2023-07-22 09:51:52 +0000

    Added Navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,15 +6,20 @@ import Button from 'react-bootstrap/Button';
 
 function Navigation() {
   return (
-    <Container>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col md="auto">WARRANTY</Col>
-        <Col xs lg="2">
-          <Button>DESIGN MY HOME</Button>
+    <>
+      <Row className=' my-div pt-3'>
+        <Col lg={1}></Col>
+          <Col>
+            <p className='pt-2 pb-2'>Talk to us Please don’t hesitate to call us on <b>+255 800 750 194</b></p></Col>
+          <Col md="auto">
+            <Button className='pt-2 pb-2'>BUY NOW</Button>
+          </Col>
+          <Col md='auto'>
+            <Button className='pt-2 pb-2'>DESIGN MY HOME</Button>
         </Col>
+        <Col lg={1}></Col>
       </Row>
-    </Container>
+    </>
   );
 }
 
